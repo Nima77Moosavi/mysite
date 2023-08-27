0a4b42aa36be4d49676a9710bb5339ecc03e4887
Nima77Moosavi <nima77moosavi@gmail.com>
2023-08-27 12:26:05 +0330

    created first app
    created first app named website
    contact,about,index views

--- mysite/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse, JsonResponse
-
-def http_test(request):
-    return HttpResponse("http-test")
-
-def json_test(request):
-    return JsonResponse({"Name":"Nima"})
